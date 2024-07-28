8925c53244be76be3141755f8e9d6f6250efd46a
Supeer <138587508+Supeer788@users.noreply.github.com>
2024-07-28 21:13:48 +0400

    Day 1 :3
    im leawning c++ to make malware also can u c deez nu-

---
+++ C++_Learning/Variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int x; //declaration
+
+    x = 69; //assignation
+
+    std::cout << x ; //print value
+
+    return 0; //operation successful
+}
---
+++ C++_Learning/datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    //integer (whole decimal)
+    int Age = 17 ;
+    //double (number inculding decimal)
+    double price = 5.99 ;
+    //char (single character)
+    char Grade = 'A' ;
+    //Bool (True / False)
+    bool Student = true ;
+    //String (sequence of char)
+    std::string name = "Tamer" ;
+
+    std :: cout << name << ' ' << Grade ;
+    return 0;
+}
