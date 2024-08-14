5c093f55667ea9c2a960209380a3587cf357c669
Supeer <22rlol@proton.me>
2024-08-14 09:15:33 +0400

    meow

---
+++ Python day3/1.py
@@ -0,0 +1,2 @@
+number = input("what number you like the most bbg:")
+print(f"{number} is a number")
---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int x, y;
+    printf("\nx: ");
+    scanf("%d",&x);
+    printf("\ny: ");
+    scanf("%d",&y);
+
+    printf("\n%d\n",x + y);
+
+}
